fe66d6fadf2c30e3ac9ef4036fac58ce4d401f03
salamov <Myself_123>
2020-05-14 14:55:58 +0300

    Выполнил первое домашнее задание.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,55 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        storage = null;
     }
 
     void save(Resume r) {
+        int count = 0;
+        for (int a = 0; a < storage.length; a++)
+            if (storage[a] != null) {
+                count++;
+            }
+
+        storage[count] = r;
     }
 
     Resume get(String uuid) {
+        for (Resume arr: storage){
+            if (arr.uuid == uuid) {
+                return arr;
+            } else {
+                return null;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int count = 0;
+        for (int a = 0; a < storage.length; a++)
+            if (storage[a] != null) {
+                count++;
+            }
+
+        for (int b = 0; b < count; b++){
+            if (storage[b].uuid == uuid) {
+                storage[b] = null;
+                storage[b] = storage[count];
+                storage[count] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return storage;
+        /*return new Resume[0];*/
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
