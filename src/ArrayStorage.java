51dc11904eca632603b5af32f500ff698c9932ff
salamov <Myself_123>
2020-05-17 20:58:59 +0300

    Исправление замечаний по первому домашнему заданию.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,7 +5,9 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        storage = null;
+        for (int a=0; a<size(); a++){
+            storage[a] = null;
+        }
     }
 
     void save(Resume r) {
@@ -19,28 +21,19 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for (Resume arr: storage){
-            if (arr.uuid == uuid) {
-                return arr;
-            } else {
-                return null;
+        for (int a = 0; a<size(); a++)
+            if (storage[a].uuid == uuid){
+                return storage[a];
             }
-        }
         return null;
     }
 
     void delete(String uuid) {
-        int count = 0;
-        for (int a = 0; a < storage.length; a++)
-            if (storage[a] != null) {
-                count++;
-            }
+        int count = size();
 
         for (int b = 0; b < count; b++){
             if (storage[b].uuid == uuid) {
                 storage[b] = null;
-                storage[b] = storage[count];
-                storage[count] = null;
             }
         }
     }
@@ -49,11 +42,22 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return storage;
+        Resume[] array = new Resume[size()];
+
+        for (int a=0; a<size(); a++)
+            array[a] = storage[a];
+
+        return array;
         /*return new Resume[0];*/
     }
 
     int size() {
-        return storage.length;
+        int count = 0;
+        for (int a = 0; a < storage.length; a++)
+            if (storage[a] != null) {
+                count++;
+            }
+
+        return count;
     }
 }
